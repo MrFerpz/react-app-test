bdb8f69f42749352c2a43dc7decde8075c8ebc5d
Fred <leferpz@gmail.com>
2024-10-23 07:41:23 -0400

    Trying conditional rendering

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -3,10 +3,10 @@ function Greeting() {
       <div>
         <h1>Test title</h1>
         <svg>
-          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+          <circle cx="50" cy="50" r="20" stroke="green" strokeWidth="2" />
         </svg>
         <form>
-          <input type="text" />
+          <input type="text"/>
         </form>
       </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
+import { Listy } from "./List.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting />
+    {/* <Greeting />
+    <App /> */}
+    <Listy />
   </StrictMode>,
 );
